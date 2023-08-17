b15e8dec69b16132fb30253d10d8b33a5aee519a
Denis Shirshov <denis.shirshov@gmail.com>
2023-08-17 09:33:39 +0300

    3

--- compile.rkt
+++ compile.rkt
@@ -10,4 +10,14 @@
 (define (compile-ttl input-tabtree output-turtle)
   (write-file output-turtle (tabtree->rdf (parse-tabtree input-tabtree))))
 
-(compile-ttl "/home/denis/projects/food-industry-kgr/source/food_industry.tree" "/home/denis/projects/food-industry-kgr/turtle/food_industry.ttl")
+; (compile-ttl "/home/denis/projects/food-industry-kgr/source/food_industry.tree" "/home/denis/projects/food-industry-kgr/turtle/food_industry.ttl")
+
+; (compile-ttl "/home/denis/projects/ontologies/tabtree/tabtree/tabtree.tree" "/home/denis/projects/ontologies/turtle/tabtree/core.ttl")
+; (compile-ttl "/home/denis/projects/ontologies/tabtree/tabtree/tabtree-abstract.tree" "/home/denis/projects/ontologies/turtle/tabtree/abstract.ttl")
+; (compile-ttl "/home/denis/projects/ontologies/tabtree/tabtree/tabtree-metrics.tree" "/home/denis/projects/ontologies/turtle/tabtree/metrics.ttl")
+; (compile-ttl "/home/denis/projects/ontologies/tabtree/tabtree/tabtree-time.tree" "/home/denis/projects/ontologies/turtle/tabtree/time.ttl")
+; (compile-ttl "/home/denis/projects/ontologies/tabtree/tabtree/tabtree-location.tree" "/home/denis/projects/ontologies/turtle/tabtree/location.ttl")
+(compile-ttl "/home/denis/projects/ontologies/tabtree/tabtree/tabtree-person.tree" "/home/denis/projects/ontologies/turtle/tabtree/person.ttl")
+; (compile-ttl "/home/denis/projects/ontologies/tabtree/tabtree/tabtree-organization.tree" "/home/denis/projects/ontologies/turtle/tabtree/organization.ttl")
+; (compile-ttl "/home/denis/projects/ontologies/tabtree/tabtree/tabtree-source.tree" "/home/denis/projects/ontologies/turtle/tabtree/source.ttl")
+; (compile-ttl "/home/denis/projects/ontologies/tabtree/tabtree/tabtree-media.tree" "/home/denis/projects/ontologies/turtle/tabtree/media.ttl")
